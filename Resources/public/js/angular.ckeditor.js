ba416a45b7bc81c4b733ee19bc44fa90b1e09b8d
Manuel Aguirre <programador.manuel@gmail.com>
2013-07-04 11:40:38 -0430

    agregando el ckeditor y un composer.json

---
+++ Resources/public/js/angular.ckeditor.js
@@ -0,0 +1,27 @@
+angular.module('ck', []).directive('ckEditor', function() {
+    return {
+        require: '?ngModel',
+        link: function(scope, elm, attr, ngModel) {
+            var ck = CKEDITOR.replace(elm[0], {
+                toolbar: [
+                    ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
+                    ['FontSize', 'TextColor', 'BGColor']
+                ]
+            });
+
+            if (!ngModel)
+                return;
+
+            ck.on('pasteState', function() {
+                scope.$apply(function() {
+                    ngModel.$setViewValue(ck.getData());
+                });
+            });
+
+            ngModel.$render = function(value) {
+                ck.setData(ngModel.$viewValue);
+            };
+        }
+    };
+});
+
